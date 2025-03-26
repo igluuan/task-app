31456e9dc09085a68affd06e6aa4995d58279e72
helpdesk - pc <contatoluansantos7@gmail.com>
2025-03-26 15:16:43 -0300

    Initialize Spring Boot project with Maven wrapper and basic configuration. Added .gitignore, .gitattributes, and essential project files including pom.xml, application properties, and main application class.

---
+++ src/test/java/tech/devluan/task_app/TaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devluan.task_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
